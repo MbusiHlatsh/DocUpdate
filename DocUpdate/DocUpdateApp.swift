744f05f9d09529f3f7da67a72a23217733779bb2
MbusiHlatsh <mbusihlatsh@gmail.com>
2026-02-22 22:00:17 -0800

    Initial Commit

---
+++ DocUpdate/DocUpdateApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocUpdateApp.swift
+//  DocUpdate
+//
+//  Created by Mbusi Hlatshwayo on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct DocUpdateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
